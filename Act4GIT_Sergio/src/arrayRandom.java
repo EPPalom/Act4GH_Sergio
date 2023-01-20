826e1ca3f6a94791d414497bd71f986e8e6f3155
goldbergcode <vertedor86@gmail.com>
2023-01-20 14:39:46 +0100

    Primer commit!

---
+++ Act4GIT_Sergio/src/arrayRandom.java
@@ -0,0 +1,14 @@
+
+public class arrayRandom {
+
+  private int[] numeros = new int[20];
+  private int longitudMax = 10;
+  private int longitudMin = 1;
+
+  public void generador() {
+    for (int i = 0; i < numeros.length; i++) {
+      numeros[i] = (int) (Math.random() * (longitudMax - longitudMin + 1) + longitudMin);
+      System.out.println(numeros[i] + " ");
+    }
+  }
+}
